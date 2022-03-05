8fc20b8b824694c39d6ac9e80dfbacbfd8e0883a
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-03-05 12:30:06 +0100

    Initialize Database Connection

--- src/core/server/events/resourceStart.ts
+++ src/core/server/events/resourceStart.ts
@@ -1,7 +1,16 @@
 import * as alt from 'alt-server';
 import { autoReconnect } from '../utility/reconnect';
+import Database from '@stuyk/ezmongodb';
+
+const url = process.env.MONGO_URL;
+const dbName = 'livingyou';
+const collections = ['accounts'];
 
 alt.on('resourceStart', async (errored: boolean) => {
+    const connected = await Database.init(url, dbName, collections);
+    if (!connected) {
+        throw new Error(`Did not connect to the database.`);
+    }
 
     //Bottom Code Line
     if (!errored && alt.debug) await autoReconnect();
