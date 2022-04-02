cedeb8475497f9013f155fcbf3c23523cfd3d175
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-03 00:28:29 +0200

    Finished Character Appearence Interface

--- src/core/shared/interface/ICharacterAppearence.ts
+++ src/core/shared/interface/ICharacterAppearence.ts
@@ -1,3 +1,36 @@
+import * as alt from 'alt-server';
+
 export default interface ICharacterAppearence {
-    
+    headBlendData: {
+        shapeFirstID: number;
+        shapeSecondID: number;
+        shapeThirdID: number;
+        skinFirstID: number;
+        skinSecondID: number;
+        skinThirdID: number;
+        shapeMix: number;
+        skinMix: number;
+        thirdMix: number;
+    };
+    headBlendPaletteColor: {
+        id: number;
+        rgb: alt.RGBA;
+    };
+    faceFeature: {
+        index: number;
+        scale: number;
+    };
+    headOverlay: {
+        overlayID: number;
+        index: number;
+        opacity: number;
+    };
+    headOverlayColor: {
+        overlayID: number;
+        colorType: number;
+        colorIndex: number;
+        secondColorIndex: number;
+    };
+    eyeColor: number;
+    hairColor: number;
 }
