e6d3e6dcf2318ed9e49fff69160a297a69d9f312
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-03 15:53:34 +0200

    Improved Scene Loading at Coords

--- src/core/client/utility/scene.ts
+++ src/core/client/utility/scene.ts
@@ -1,30 +1,32 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
 
-export function loadSceneAtCoords(pos: alt.Vector3): Promise<boolean> {
+export async function loadSceneAtCoords(pos: alt.Vector3): Promise<boolean> {
     let timerHandle: number;
-    return new Promise<boolean>((resolve) => {
-        native.newLoadSceneStartSphere(
-            pos.x,
-            pos.y,
-            pos.z ?? native.getHeightmapBottomZForPosition(pos.x, pos.z),
-            2,
-            1
-        );
+    try {
+        return await new Promise<boolean>((resolve) => {
+            native.newLoadSceneStartSphere(
+                pos.x,
+                pos.y,
+                pos.z ?? native.getHeightmapBottomZForPosition(pos.x, pos.z),
+                2,
+                1
+            );
 
-        timerHandle = alt.setInterval(() => {
-            if (!native.isNewLoadSceneActive()) {
-                return resolve(false);
-            }
+            timerHandle = alt.setInterval(() => {
+                if (!native.isNewLoadSceneActive()) {
+                    return resolve(false);
+                }
 
-            if (!native.isNewLoadSceneLoaded()) {
-                return;
-            }
+                if (!native.isNewLoadSceneLoaded()) {
+                    return;
+                }
 
-            return resolve(true);
-        }, 10);
-    }).finally(() => {
+                return resolve(true);
+            }, 10);
+        });
+    } finally {
         native.newLoadSceneStop();
         alt.clearInterval(timerHandle);
-    });
+    }
 }
