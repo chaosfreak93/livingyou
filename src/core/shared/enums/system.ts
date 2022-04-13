a635702ea26a5a4fcf7ae0df23f929fe2c3536d0
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-13 22:42:19 +0200

    SYSTEM_EVENTS enum

---
+++ src/core/shared/enums/system.ts
@@ -0,0 +1,7 @@
+export enum SYSTEM_EVENTS {
+    BEGIN_CONNECTION = 'connection:Begin',
+    DISCORD_OPEN = 'discord:Open',
+    DISCORD_LOGIN = 'discord:Login',
+    DISCORD_FINISH_AUTH = 'discord:FinishAuth',
+    WEBVIEW_INFO = 'webview:Info',
+}
