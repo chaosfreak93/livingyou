669abe6cc6ddfccfcb33f0fb3c866069e58167b5
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-13 22:50:02 +0200

    New Webview Controller for the new Webview Setup

--- src/core/client/startup.ts
+++ src/core/client/startup.ts
@@ -1,6 +1,7 @@
 import './events/connectionComplete';
 import './events/consoleCommand';
 import './events/disconnect';
+import './extensions/webViewController';
 import './systems/cameraManager';
 import './systems/charSelector';
 import './systems/iplmanager';
