7f7173cb976ff1b3ca6962491693b8193f96b9f0
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-13 22:45:39 +0200

    Load Camera Manager

--- src/core/client/startup.ts
+++ src/core/client/startup.ts
@@ -1,5 +1,6 @@
 import './events/connectionComplete';
 import './events/consoleCommand';
 import './events/disconnect';
+import './systems/cameraManager';
 import './systems/charSelector';
 import './systems/iplmanager';
