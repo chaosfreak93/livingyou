53b7223aef75f9848f3044a48d294fd69b5ceb4b
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-15 01:51:49 +0200

    Added new CHAR_SELECTOR_OPEN Event

--- src/core/shared/enums/system.ts
+++ src/core/shared/enums/system.ts
@@ -1,7 +1,11 @@
 export enum SYSTEM_EVENTS {
     BEGIN_CONNECTION = 'connection:Begin',
+    // Discord Login
     DISCORD_OPEN = 'discord:Open',
     DISCORD_LOGIN = 'discord:Login',
     DISCORD_FINISH_AUTH = 'discord:FinishAuth',
+    // Character Selector
+    CHAR_SELECTOR_OPEN = 'charSelector:Open',
+    // Webview
     WEBVIEW_INFO = 'webview:Info',
 }
