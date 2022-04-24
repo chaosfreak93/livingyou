5c7036abf6c27739960fa88b764d07976fb0ae68
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-24 19:48:41 +0200

    Finished Char Selector (Serverside)

--- src/core/server/systems/account.ts
+++ src/core/server/systems/account.ts
@@ -3,6 +3,7 @@ import Database from '@stuyk/ezmongodb';
 import IAccount from '../interface/IAccount';
 import IDiscordData from '../interface/IDiscordData';
 import { SYSTEM_EVENTS } from '../../shared/enums/system';
+import ICharacter from '../../shared/interface/ICharacter';
 
 alt.on(SYSTEM_EVENTS.DISCORD_FINISH_AUTH, authFinished);
 
@@ -35,4 +36,71 @@ async function authFinished(player: alt.Player, discordData: IDiscordData): Prom
         findAccount[0].character,
         findAccount[0].allowSecondCharacter
     );
+}
+
+alt.onClient(SYSTEM_EVENTS.CHAR_SELECTOR_SELECT_CHAR, selectChar);
+
+async function selectChar(player: alt.Player, character: ICharacter) {
+    player.character = character;
+    player.dimension = 0;
+    player.visible = true;
+    player.setHeadBlendData(
+        character.characterAppearence.headBlendData.mother,
+        character.characterAppearence.headBlendData.father,
+        0,
+        character.characterAppearence.headBlendData.mother,
+        character.characterAppearence.headBlendData.father,
+        0,
+        character.characterAppearence.headBlendData.similarityAnatomy,
+        character.characterAppearence.headBlendData.similaritySkinColor,
+        0
+    );
+    for (let i = 0; i < character.characterAppearence.faceFeature.length; i++) {
+        player.setFaceFeature(i, parseFloat(character.characterAppearence.faceFeature[i].scale));
+    }
+    for (let i = 0; i < character.characterAppearence.headOverlay.length; i++) {
+        player.setHeadOverlay(
+            i,
+            parseInt(character.characterAppearence.headOverlay[i].index),
+            parseFloat(character.characterAppearence.headOverlay[i].opacity)
+        );
+    }
+    for (let i = 0; i < character.characterAppearence.headOverlay.length; i++) {
+        player.setHeadOverlayColor(
+            i,
+            parseInt(character.characterAppearence.headOverlay[i].colorType),
+            parseInt(character.characterAppearence.headOverlay[i].colorIndex),
+            0
+        );
+    }
+    player.setEyeColor(parseInt(character.characterAppearence.eyeColor));
+    player.setHairColor(parseInt(character.characterAppearence.hairColor.colorId));
+    player.setHairHighlightColor(parseInt(character.characterAppearence.hairColor.highlightColorId));
+
+    for (let i = 0; i < character.characterClothing.clothes.length; i++) {
+        player.setClothes(
+            parseInt(character.characterClothing.clothes[i].component),
+            parseInt(character.characterClothing.clothes[i].drawable),
+            parseInt(character.characterClothing.clothes[i].texture),
+            0
+        );
+    }
+
+    for (let i = 0; i < character.characterClothing.props.length; i++) {
+        if (character.characterClothing.props[i].drawable == -1) {
+            player.clearProp(character.characterClothing.props[i].component);
+        } else {
+            player.setProp(
+                parseInt(character.characterClothing.props[i].component),
+                parseInt(character.characterClothing.props[i].drawable),
+                parseInt(character.characterClothing.props[i].texture)
+            );
+        }
+    }
+
+    player.setPosition(player, -1037.98681640625, -2738.3076171875, 20.1640625);
+
+    alt.emitClient(player, SYSTEM_EVENTS.CHAR_SELECTOR_CLOSE);
+    await alt.Utils.wait(500);
+    alt.emitClient(player, SYSTEM_EVENTS.HUD_OPEN);
 }
