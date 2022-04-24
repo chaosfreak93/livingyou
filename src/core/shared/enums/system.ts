fee352d30eb6a433fa24c582ed4aa3fb8f2deab0
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-24 19:45:57 +0200

    Added new Events

--- src/core/shared/enums/system.ts
+++ src/core/shared/enums/system.ts
@@ -6,9 +6,14 @@ export enum SYSTEM_EVENTS {
     DISCORD_FINISH_AUTH = 'discord:FinishAuth',
     // Character Selector
     CHAR_SELECTOR_OPEN = 'charSelector:Open',
+    CHAR_SELECTOR_SELECT_CHAR = 'charSelector:selectChar',
+    CHAR_SELECTOR_CLOSE = 'charSelector:Close',
     // Character Creator
     CHAR_CREATOR_OPEN = 'charCreator:Open',
+    CHAR_CREATOR_CLOSE = 'charCreator:Close',
     CHAR_CREATOR_FINISH_CHAR = 'charCreator:finishChar',
+    // HUD
+    HUD_OPEN = 'hud:Open',
     // Webview
     WEBVIEW_INFO = 'webview:Info',
 }
