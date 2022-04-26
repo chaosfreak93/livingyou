4ac6e2177da049d30500c21f4d3baa8624dc230a
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-04-26 09:19:32 +0200

    Added HUD Webview

--- src-webviews/src/pages/components.ts
+++ src-webviews/src/pages/components.ts
@@ -2,6 +2,7 @@ import { shallowRef } from 'vue';
 import Login from './login/Login.vue';
 import CharSelector from './charSelector/CharSelector.vue';
 import CharCreator from './charCreator/CharCreator.vue';
+import HUD from './hud/HUD.vue';
 
 // Append components here...
 // All components that you want to load...
@@ -10,6 +11,7 @@ const componentList = {
     Login: shallowRef(Login),
     CharSelector: shallowRef(CharSelector),
     CharCreator: shallowRef(CharCreator),
+    HUD: shallowRef(HUD),
 };
 
 function generateComponentList(): Array<{ name: string; component: unknown }> {
