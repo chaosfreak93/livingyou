d4ddf9d932c7e759dea6b7cf0ca8af9a706283a6
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-05-25 23:27:56 +0200

    Extended Item Interface

--- resources/livingyou/shared/interface/IItem.ts
+++ resources/livingyou/shared/interface/IItem.ts
@@ -1,5 +1,20 @@
 export default interface IItem {
-    type: 'Consumable';
+    type: 'Consumable', 'Money';
     name: string;
     weigth: number;
+    data?: {
+        food?: number;
+        thirst?: number;
+        effect?: {
+            name: string;
+            duration: number;
+        };
+        protection?: number;
+        animation?: {
+            dictionary: string;
+            name: string;
+            duration: number;
+            flags: number;
+        }
+    }
 }
