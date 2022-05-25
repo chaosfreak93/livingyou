b532f1e62d7c7b18b82803a7eb8878dcb210254d
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-05-25 23:36:15 +0200

    Started Inventory Webview

--- resources/webviews/client-src/pages/inventory/Inventory.vue
+++ resources/webviews/client-src/pages/inventory/Inventory.vue
@@ -1,5 +1,11 @@
 <template>
-    <div></div>
+    <div>
+        <h1 id="title">{{ title }}</h1>
+        <p id="weight">{{ current_weight }}kg / {{ max_weight }}kg</p>
+        <div id="itemlist">
+            <div id="item" v-for="item in getItems" v-bind:key="item.index"></div>
+        </div>
+    </div>
 </template>
 
 <script lang="ts">
@@ -9,13 +15,59 @@ export default defineComponent({
     name: ComponentName,
     components: {},
     data() {
-        return {};
+        return {
+            title: '',
+            current_weight: 0,
+            max_weight: 50,
+            items: [],
+        };
     },
-    methods: {},
+    methods: {
+        setData(title, current_weight, max_weight, items) {
+            this.title = title;
+            this.current_weight = current_weight;
+            this.max_weight = max_weight;
+            this.items = items;
+        },
+    },
+    computed: {
+        getItems() {
+            return this.items;
+        },
+    },
+    mounted() {
+        if (`alt` in window) {
+            alt.emit('inventoryReady');
+        }
+    },
 });
 </script>
 
 <style scoped>
 #page-Inventory {
+    text-align: center;
+    position: absolute;
+    top: 2.5%;
+    left: 1%;
+    transform: translate(0%, 0%);
+    width: 30vw;
+    min-height: 95%;
+    background-color: rgba(0, 0, 0, 0.55);
+    border-radius: 10px;
+}
+
+#itemlist {
+    margin: 0px 25px;
+    display: grid;
+    grid-template-columns: auto auto auto auto auto;
+    gap: 10px;
+    justify-content: center;
+    align-items: center;
+}
+
+#item {
+    width: 70px;
+    height: 74px;
+    background-color: #ffffff;
 }
 </style>
