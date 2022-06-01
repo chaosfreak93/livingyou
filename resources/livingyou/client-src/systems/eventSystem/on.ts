0c183ae5246a2fdd21f2dd99e6bfb08492a1af6c
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-01 22:13:50 +0200

    New Event Listener

--- resources/livingyou/client-src/systems/eventSystem/on.ts
+++ resources/livingyou/client-src/systems/eventSystem/on.ts
@@ -7,13 +7,26 @@ export function On(eventName: keyof alt.IClientEvent): Function {
     };
 }
 
+export function Once(eventName: keyof alt.IClientEvent): Function {
+    return (target: Object, _propertyKey: string, descriptor: PropertyDescriptor) => {
+        alt.once(eventName, descriptor.value);
+    };
+}
+
 export function OnServer(eventName: keyof ISystemEvents): Function {
     return (target: Object, _propertyKey: string, descriptor: PropertyDescriptor) => {
         alt.onServer(eventName, descriptor.value);
     };
 }
 
+export function OnceServer(eventName: keyof ISystemEvents): Function {
+    return (target: Object, _propertyKey: string, descriptor: PropertyDescriptor) => {
+        alt.onceServer(eventName, descriptor.value);
+    };
+}
+
 export default {
     On,
     OnServer,
+    OnceServer,
 };
