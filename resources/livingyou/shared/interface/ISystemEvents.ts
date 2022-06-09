3b09a2d94c34d2b9c6737f5849d3706305727648
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-09 23:27:55 +0200

    Fixed Type Errors

--- resources/livingyou/shared/interface/ISystemEvents.ts
+++ resources/livingyou/shared/interface/ISystemEvents.ts
@@ -1,31 +1,34 @@
+import IInventory from './IInventory';
+
 export interface ISystemEvents {
-    'connection:Begin',
+    'connection:Begin': () => void;
     // Discord Login
-    'discord:Open': () => void,
-    'discord:ProceedToken',
-    'discord:FinishAuth',
-    'discord:Close': () => void,
+    'discord:Open': () => void;
+    'discord:ProceedToken': (token: string) => void;
+    'discord:Close': () => void;
     // Character Selector
-    'charSelector:Open': () => void,
-    'charSelector:SelectChar',
-    'charSelector:Close': () => void,
+    'charSelector:Open': () => void;
+    'charSelector:SelectChar': (character: string) => void;
+    'charSelector:Close': () => void;
     // Character Creator
-    'charCreator:Open': () => void,
-    'charCreator:FinishChar',
-    'charCreator:Close': () => void,
+    'charCreator:Open': () => void;
+    'charCreator:FinishChar': (character: string) => void;
+    'charCreator:Close': () => void;
     // HUD
-    'hud:Open',
+    'hud:Open': () => void;
+    // Inventory
+    'inventory:Open': (inventory: IInventory) => void;
     // Webview
-    'webView:Info',
+    'webView:Info': (url: string) => void;
     // KeyManager
-    'keyManager:KeyUp',
-    'keyManager:KeyDown',
+    'keyManager:KeyUp': (key: number) => void;
+    'keyManager:KeyDown': (key: number) => void;
     // Ticks
-    'player:StartTicks',
-    'player:Tick',
+    'player:StartTicks': () => void;
+    'player:Tick': () => void;
     // World
-    'world:UpdateTime',
-    'world:UpdateWeather',
+    'world:UpdateTime': (hour: number, minute: number) => void;
+    'world:UpdateWeather': (weather: string) => void;
     // Dev Tools
-    'debug',
+    'debug': () => void;
 }
