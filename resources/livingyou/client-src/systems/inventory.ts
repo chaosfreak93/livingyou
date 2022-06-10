b3fce23499af7fe9e2ee8176b30b074087efe2c6
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-10 13:08:50 +0200

    Ooops

--- resources/livingyou/client-src/systems/inventory.ts
+++ resources/livingyou/client-src/systems/inventory.ts
@@ -1,11 +1,9 @@
 import * as alt from 'alt-client';
 import * as native from 'natives';
-import { WebViewController } from '../../client-src/extensions/webViewController';
+import { WebViewController } from '../extensions/webViewController';
 
 export default class Inventory {
     static async open(): Promise<void> {
         const view = await WebViewController.get();
-
-        WebViewController.
     }
 }
