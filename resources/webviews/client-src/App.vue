a36e17fe5406a7ee11185df700457313de5881af
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-16 16:56:13 +0200

    Fixed Webview Viewport

--- resources/webviews/client-src/App.vue
+++ resources/webviews/client-src/App.vue
@@ -245,4 +245,19 @@ export default defineComponent({
     background: rgba(0, 0, 0, 0.2);
     cursor: pointer;
 }
+
+html,
+body {
+  margin: 0;
+  padding: 0;
+}
+
+body {
+  height: 100vh;
+  width: 100vw;
+}
+
+* {
+  box-sizing: border-box;
+}
 </style>
