7a605d7102e9a60f7261eb0c95b6b87400bf6a9b
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-20 11:28:34 +0200

    Fixed Screen Fader

--- resources/livingyou/client-src/utility/screenFade.ts
+++ resources/livingyou/client-src/utility/screenFade.ts
@@ -4,7 +4,7 @@ import * as native from 'natives';
 export default class ScreenFade {
     static async fadeIn(duration: number): Promise<void> {
         return new Promise<void>(async (resolve, reject) => {
-            if (!native.isScreenFadedOut() || native.isScreenFadingIn() || native.isScreenFadingOut()) return resolve();
+            if (!native.isScreenFadedOut() || native.isScreenFadingIn()) return resolve();
             native.doScreenFadeIn(duration);
             await alt.Utils.waitFor(() => native.isScreenFadedIn());
             return resolve();
@@ -13,7 +13,7 @@ export default class ScreenFade {
 
     static async fadeOut(duration: number): Promise<void> {
         return new Promise<void>(async (resolve, reject) => {
-            if (!native.isScreenFadedIn() || native.isScreenFadingIn() || native.isScreenFadingOut()) return resolve();
+            if (!native.isScreenFadedIn() || native.isScreenFadingOut()) return resolve();
             native.doScreenFadeOut(duration);
             await alt.Utils.waitFor(() => native.isScreenFadedOut());
             return resolve();
