a61b43f691f22f73a854298d10ab276f3a477580
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-22 17:58:36 +0200

    Fixed Key Manager

--- resources/livingyou/server-src/systems/keyManager.ts
+++ resources/livingyou/server-src/systems/keyManager.ts
@@ -5,6 +5,7 @@ import { OnClient } from './eventSystem/on';
 export default class KeyManager {
     @OnClient('keyManager:KeyUp')
     static keyUp(player: alt.Player, key: number): void {
+        if (!player.character) return;
         switch (key) {
             case 112:
                 EmitClient(player, 'devTools:debug');
@@ -25,6 +26,7 @@ export default class KeyManager {
 
     @OnClient('keyManager:KeyDown')
     static keyDown(player: alt.Player, key: number): void {
+        if (!player.character) return;
         switch (key) {
             default:
                 break;
