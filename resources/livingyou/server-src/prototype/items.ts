07e2c69f7124ce0687eff5a5a9bde3a9048d95ea
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-22 22:22:00 +0200

    Added new Items Methods

--- resources/livingyou/server-src/prototype/items.ts
+++ resources/livingyou/server-src/prototype/items.ts
@@ -4,6 +4,19 @@ import IItem from '../../shared/interface/IItem';
 
 export default class Items {
     static items: IItem[];
+
+    static getItemByName(name: string): IItem | null {
+        return this.items.find((value: IItem) => {
+            value.name == name;
+        });
+    }
+
+    static getItemById(id: string): IItem | null {
+        return this.items.find((value: IItem) => {
+            value.id == id;
+        });
+    }
+
     static async fetchItems(): Promise<void> {
         Items.items = await Database.fetchAllData<IItem>('items');
     }
