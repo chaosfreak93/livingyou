3fd9ed06ac17ebd47421c53a8fb1ec7130e480d0
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-23 16:49:58 +0200

    HUD: Fix Vehicle Speed

--- resources/livingyou/client-src/systems/hud.ts
+++ resources/livingyou/client-src/systems/hud.ts
@@ -66,7 +66,7 @@ export default class HUD {
             view.emit(
                 'updateVehicleData',
                 alt.Player.local.vehicle.rpm,
-                (alt.Player.local.vehicle.speed * 3.6).toFixed(0)
+                (native.getEntitySpeed(alt.Player.local.vehicle) * 3.6).toFixed(0)
             );
         });
     }
