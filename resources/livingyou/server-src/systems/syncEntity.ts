5cc150a8a61b513500dccf3b217938c9b9d5ae9c
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-06-23 16:51:59 +0200

    EntitySync: Increase Stream Delay

--- resources/livingyou/server-src/systems/syncEntity.ts
+++ resources/livingyou/server-src/systems/syncEntity.ts
@@ -1,7 +1,7 @@
 import * as xsync from 'altv-xsync-entity-server';
 
 new xsync.XSyncEntity({
-    streamDelay: 100,
+    streamDelay: 200,
     wss: {
         port: 19132,
         localhost: true,
