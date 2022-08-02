7e350ccb0e5182de881982707ea38849b165224f
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-08-02 22:11:28 +0200

    Added Gas Pumps Blips

--- resources/livingyou/server-src/systems/gasPumps.ts
+++ resources/livingyou/server-src/systems/gasPumps.ts
@@ -22,6 +22,31 @@ export default class GasPumps {
 
     static async fetchGasPumps(): Promise<void> {
         GasPumps.gasPumps = await Database.fetchAllData<IGasPump>(DBCollections.GAS_PUMPS);
+        for (let i = 0; i < GasPumps.gasPumps.length; i++) {
+            let gasPumpBlip = new alt.PointBlip(
+                this.gasPumps[i].position.x,
+                this.gasPumps[i].position.y,
+                this.gasPumps[i].position.z
+            );
+            let name: string = 'Zapfsäule - ';
+            if (this.gasPumps[i].fuelType.diesel) {
+                name += 'Diesel';
+                gasPumpBlip.color = 17;
+            }
+            if (this.gasPumps[i].fuelType.benzin) name += ', Benzin';
+            if (this.gasPumps[i].fuelType.eletric) {
+                name += 'Strom';
+                gasPumpBlip.color = 5;
+            }
+            if (this.gasPumps[i].fuelType.cerosin) name += 'Kerosin';
+            if (this.gasPumps[i].fuelType.air) {
+                name += 'Luft';
+                gasPumpBlip.color = 3;
+            }
+            if (this.gasPumps[i].fuelType.water) name += ', Wasser';
+            gasPumpBlip.name = name;
+            gasPumpBlip.sprite = 361;
+        }
         alt.log(`~lk~[~y~LivingYou~lk~] ~b~GasPumps - ${GasPumps.gasPumps.length}~w~`);
     }
 }
