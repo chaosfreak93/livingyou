df4a58837a65247c4243f67a90924321c8c423ef
chaosfreak93 <yanluca.lentzen@gmail.com>
2022-08-03 22:13:55 +0200

    Cleanup HUD

--- resources/livingyou/shared/enums/WebViewEvents.ts
+++ resources/livingyou/shared/enums/WebViewEvents.ts
@@ -7,4 +7,9 @@ export const enum WebViewEvents {
     CHAR_SELECTOR_SHOW_PED = 'charSelector:ShowPed',
     CHAR_SELECTOR_OPEN_CHAR_CREATOR = 'charSelector:OpenCharCreator',
     CHAR_SELECTOR_SELECT_CHARACTER = 'charSelector:SelectCharacter',
+    // HUD
+    HUD_READY = 'hud:Ready',
+    HUD_OPEN_VEHICLE_HUD = 'hud:OpenVehicleHUD',
+    HUD_CLOSE_VEHICLE_HUD = 'hud:CloseVehicleHUD',
+    HUD_UPDATE_VEHICLE_DATA = 'hud:UpdateVehicleData',
 }
