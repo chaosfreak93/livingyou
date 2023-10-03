e48aa0157f2c9d284fe2bc261ea3d253a96b77ad
KeinerTTV <yanluca.lentzen@gmail.com>
2023-10-03 10:45:00 +0200

    Added new RPC Event Mehods

--- resources/livingyou/client-src/systems/eventSystem/emit.ts
+++ resources/livingyou/client-src/systems/eventSystem/emit.ts
@@ -5,6 +5,11 @@ export function EmitServer(eventName: keyof ISystemEvents, ...args: any[]): void
     alt.emitServerRaw(eventName, ...args);
 }
 
+export function EmitRPC(eventName: keyof ISystemEvents, ...args: any[]): Promise<unknown> {
+    return alt.emitRpc(eventName, ...args);
+}
+
 export default {
     EmitServer,
+    EmitRPC
 };
